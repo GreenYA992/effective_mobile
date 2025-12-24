c335c6d2f391c589a425216a5912109020250a9c
GreenYA992 <Greenya992@gmail.com>
2025-12-24 17:11:06 +0300

    'new'

--- conftest.py
+++ conftest.py
@@ -4,16 +4,16 @@ import pytest
 from pages.login_page import LoginPage
 from utils.driver_factory import DriverFactory
 
+
 # Регистрируем маркер
 def pytest_configure(config):
     config.addinivalue_line(
-        "markers",
-        "local: Тесты для локального запуска (браузер виден)"
+        "markers", "local: Тесты для локального запуска (браузер виден)"
     )
     config.addinivalue_line(
-        "markers",
-        "docker: Тесты для запуска в Docker (headless режим)"
+        "markers", "docker: Тесты для запуска в Docker (headless режим)"
     )
+
 
 @pytest.fixture(scope="function")
 def driver(request):
@@ -24,15 +24,14 @@ def driver(request):
     if local_marker:
         headless = False  # Локально - браузер виден
     elif docker_marker:
-        headless = True   # Docker - headless
+        headless = True  # Docker - headless
     else:
-        headless = True   # По умолчанию - для совместимости с Docker
+        headless = True  # По умолчанию - для совместимости с Docker
 
-    driver = DriverFactory.get_driver(
-        headless=headless
-    )
+    driver = DriverFactory.get_driver(headless=headless)
     yield driver
     driver.quit()
+
 
 @pytest.fixture(scope="function")
 def login_page(driver):
